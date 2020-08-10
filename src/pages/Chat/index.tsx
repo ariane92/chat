645a8524cac25eab5f2dbc4703faa5322932fe80
Ariane Mateus <ariane.ccomp@gmail.com>
2020-08-10 19:02:20 -0300

    feat: chat screen

--- src/pages/Chat/index.tsx
+++ src/pages/Chat/index.tsx
@@ -1,9 +1,45 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import { View } from 'react-native';
+import { GiftedChat } from 'react-native-gifted-chat';
 import { Container } from './styles';
 
 const Chat: React.FC = () => {
-  return <View />;
+  const handleSendMessage = useCallback(messages => {
+    console.log(messages);
+  }, []);
+  const user = {
+    _id: 1,
+    name: 'Ariane',
+    avatar: 'url',
+  };
+
+  const messages = [
+    {
+      _id: 1,
+      text: 'Fala Dev',
+      createdAt: new Date(),
+      user: {
+        _id: 2,
+        name: 'Thaís',
+        avatar: 'url2',
+      },
+    },
+
+    {
+      _id: 2,
+      text: 'Boa Tarde',
+      createdAt: new Date(),
+      user: {
+        _id: 3,
+        name: 'Lele',
+        avatar: 'url2',
+      },
+    },
+  ];
+
+  return (
+    <GiftedChat user={user} messages={messages} onSend={handleSendMessage} />
+  );
 };
 
 export default Chat;
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,9 +1,52 @@
-import React from 'react';
-import { View } from 'react-native';
-import { Container } from './styles';
+import React, { useCallback } from 'react';
+import { useNavigation } from '@react-navigation/native';
+import {
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  StyleSheet,
+} from 'react-native';
 
 const Home: React.FC = () => {
-  return <View />;
+  const navigation = useNavigation();
+
+  const navigateToChat = useCallback(() => {
+    navigation.navigate('Chat');
+  }, [navigation]);
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Digite seu nome</Text>
+      <TextInput style={styles.input} />
+      <TouchableOpacity onPress={navigateToChat}>
+        <Text style={styles.button}>Entrar</Text>
+      </TouchableOpacity>
+    </View>
+  );
 };
 
 export default Home;
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 10,
+  },
+
+  text: {
+    marginTop: 10,
+    fontSize: 20,
+  },
+
+  input: {
+    marginTop: 10,
+    borderWidth: 1,
+    borderColor: '#000',
+    padding: 5,
+  },
+
+  button: {
+    marginTop: 10,
+    fontSize: 20,
+  },
+});
